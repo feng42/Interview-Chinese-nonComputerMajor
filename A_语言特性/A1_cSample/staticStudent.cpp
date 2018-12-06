80205473876ed675d33abce16e5560ec3cd7f26a
FengZhang <43266369+feng42@users.noreply.github.com>
2018-12-06 19:23:00 +0800

    Add files via upload

---
+++ A_语言特性/A1_cSample/staticStudent.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+const int MAX_NAME_SIZE=30;
+
+class Student{
+public:
+	Student(char *pszName);
+	~Student();
+public:
+	static void PrintfAllStudents();
+private:
+	char m_name[MAX_NAME_SIZE];
+	Student *next;
+	Student *prev;
+	static Student *m_head;
+};
+
+Student::Student(char *pszName){
+	strcpy(this->m_name,pszName);
+	//建立双链表，从头部插入
+	this->next=m_head;
+	this->prev=NULL;
+	if(m_head!=NULL)
+		m_head->prev=this;
+	m_head=this;
+}
+
+Student::~Student(){//析构过程就是结点的脱离过程
+	if (this==m_head)
+		m_head=this->next;
+	else{
+		this->prev->next=this->next;
+		this->next->prev=this->prev;
+	}
+}
+
+void Student::PrintfAllStudents(){
+	for (Student *p=m_head;p!=NULL;p=p->next)
+		printf("%s\n",p->m_name);
+}
+
+Student *Student::m_head=NULL;
+
+int main(){
+	Student studentA("AAA");
+	Student studentB("BBB");
+	Student studentC("CCC");
+	Student studentD("DDD");
+	Student student("MoreWindows");
+	Student::PrintfAllStudents();
+
+	return 0;
+}
